4509ed41bb90b0f46a24cd2212f648ae3c688a91
ShaguittoScot <arields.ti23@utsjr.edu.mx>
2025-03-21 11:59:56 -0600

    Arreglos para render

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // Esto asegura que Vite escuche en todas las interfaces de red
     port: process.env.PORT || 5174, // Utiliza el puerto proporcionado por Render o 5173 por defecto
+    allowedHosts: [
+        'dream2-0-1.onrender.com', // Agrega la URL de tu app de Render aquí
+      ],
   },
 });
